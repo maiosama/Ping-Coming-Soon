cca7fd084b706031b506baef031047cb432180f6
maiosama <72415874+maiosama@users.noreply.github.com>
2021-06-21 19:43:31 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+let email=document.getElementById("email");
+let notify = document.getElementsByClassName("btn")
+let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+let errorSpan = document.getElementsByClassName("message");
+
+
+notify[0].addEventListener('click', checkForm(e))
+
+
+
+    // function emptyErrorMessage(e){
+    //     e.preventDefault();
+    //     switch (email.value){
+    //     case (email.value.length<1):
+    //         errorSpan[0].innerHTML ="Whoops! It looks like you forgot to add your email";
+    //         break; 
+    //     case(!email.value.match(regexEmail)):
+    //             errorSpan[0].innerHTML = "Please provide a valid email address";
+    //             break;
+    //     default:
+    //         errorSpan[0].innerHTML=""
+    //         }
+    // }
+
+    function checkEmail(){
+        // e.preventDefault();
+        if (email.value.length<1){
+            errorSpan[0].innerHTML ="Whoops! It looks like you forgot to add your email"
+        }
+    }
+
+    function checkEntry(){
+        if(!email.value.match(regexEmail)){
+            errorSpan[0].innerHTML = "Please provide a valid email address";
+        }
+
+    }
+
+    function checkForm(e){
+        e.preventDefault();
+        var isValid = checkEmail();
+        isValid= checkEntry() && isValid;
+        return isValid
+    }
