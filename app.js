bbc88edab8c218ddd5570c73bda9bf1777d5c23a
maiosama <72415874+maiosama@users.noreply.github.com>
2021-06-24 16:28:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,44 +1,28 @@
-let email=document.getElementById("email");
-let notify = document.getElementsByClassName("btn")
+let email=document.forms["form"]["email"];
+let notify = document.getElementsByClassName("form")
 let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-let errorSpan = document.getElementsByClassName("message");
+let form=document.getElementById("form")
+let errorSpan = document.createElement("p");
+errorSpan.className="message"
 
 
-notify[0].addEventListener('submit', checkForm(e))
+notify[0].addEventListener('submit', checkForm)
 
-
-
-    // function emptyErrorMessage(e){
-    //     e.preventDefault();
-    //     switch (email.value){
-    //     case (email.value.length<1):
-    //         errorSpan[0].innerHTML ="Whoops! It looks like you forgot to add your email";
-    //         break; 
-    //     case(!email.value.match(regexEmail)):
-    //             errorSpan[0].innerHTML = "Please provide a valid email address";
-    //             break;
-    //     default:
-    //         errorSpan[0].innerHTML=""
-    //         }
-    // }
-
-    function checkEmail(){
-        // e.preventDefault();
-        if (email.value.length<1){
-            errorSpan[0].innerHTML ="Whoops! It looks like you forgot to add your email"
-        }
-    }
-
-    function checkEntry(){
-        if(!email.value.match(regexEmail)){
-            errorSpan[0].innerHTML = "Please provide a valid email address";
-        }
-
-    }
 
     function checkForm(e){
         e.preventDefault();
-        var isValid = checkEmail();
-        isValid= checkEntry() && isValid;
-        return isValid
+        if (email.value ===''){
+            form.appendChild(errorSpan)
+            form.insertBefore(errorSpan,form.childNodes[2])
+            errorSpan.innerHTML ="Whoops! It looks like you forgot to add your email"
+            email.style.borderColor="hsl(354, 100%, 66%)"
+            email.style.display="inline-block"
+        }else if (!email.value.match(regexEmail)){
+            form.appendChild(errorSpan)
+            form.insertBefore(errorSpan,form.childNodes[2])
+            errorSpan.innerHTML = "Please provide a valid email address";
+            email.style.borderColor="hsl(354, 100%, 66%)"
+
+        }
+
     }
